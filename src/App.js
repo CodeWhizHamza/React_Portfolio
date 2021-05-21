58e4a0ebd67ed424e860f6d3de4894bf973d5e34
arjungautam1 <arjungautam5431@gmail.com>
2021-05-21 22:37:04 +0545

    navbar completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+import NavbarComp from './components/NavbarComp';
+
+function App() {
+    return (
+        <div className="App">
+            <NavbarComp/>
+        </div>
+    );
+}
+
+export default App;
