1e75c1572846b02d4c6fe8043f7c119ba3895090
Hamza <muhammadhamzam1486@gmail.com>
2021-06-20 11:06:27 +0500

    SWITCH to 'hamza-contribution' branch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from "react";
-import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import NavComponent from "./components/Navbar/NavComponent";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact/Contact";
-import Home from "./components/Home/Home";
-import Footer from "./components/Footer/Footer";
-import About from "./components/About/About";
-import Youtube from "./components/Youtube/Youtube";
-import Certification from "./components/Certification/Certification";
+import React from 'react'
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import NavComponent from './components/Navbar/NavComponent'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Projects from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
+import About from './components/About/About'
+import Youtube from './components/Youtube/Youtube'
+import Certification from './components/Certification/Certification'
 
 function App() {
+  const renderWithFooter = component => {
+    return (
+      <>
+        <component />
+        <Footer />
+      </>
+    )
+  }
   return (
-    <div style={{marginBottom:"30px",marginTop:"0px",paddingBottom:"40px"}}>
+    <div
+      style={{ marginBottom: '30px', marginTop: '0px', paddingBottom: '40px' }}
+    >
       <Router>
         <div className="App">
           <NavComponent />
           <Switch>
             <Route path="/about">
               <About />
-             <Footer/>
+              <Footer />
             </Route>
             <Route path="/projects">
               <Projects />
@@ -31,11 +41,11 @@ function App() {
               <Footer />
             </Route>
             <Route path="/youtube">
-             <Youtube/>
+              <Youtube />
               <Footer />
             </Route>
             <Route path="/certification">
-             <Certification/>
+              <Certification />
               <Footer />
             </Route>
             <Route path="/">
@@ -46,7 +56,7 @@ function App() {
         </div>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
